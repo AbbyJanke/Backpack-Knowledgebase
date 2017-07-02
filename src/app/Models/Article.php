db4747c37e72f97ca71351bd64f27b6d5126748b
Abby Janke <abbyljanke@gmail.com>
2017-07-02 02:44:44 -0700

    Remove unnecessary functions from models

--- src/app/Models/Article.php
+++ src/app/Models/Article.php
@@ -46,16 +46,6 @@ class Article extends Model
     |--------------------------------------------------------------------------
     */
 
-    public function getPageLink()
-    {
-        return url($this->slug);
-    }
-
-    public function getOpenButton()
-    {
-        return '<a class="btn btn-default btn-xs" href="'.$this->getPageLink().'" target="_blank"><i class="fa fa-eye"></i> Open</a>';
-    }
-
     /*
     |--------------------------------------------------------------------------
     | RELATIONS
